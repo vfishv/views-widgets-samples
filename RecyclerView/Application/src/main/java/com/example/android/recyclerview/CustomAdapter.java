947e6fa87a71b5581dbdc1138f37e3f1a47e3d63
Q <foldervapp@gmail.com>
2023-01-27 01:24:56 +0800

    up

--- RecyclerView/Application/src/main/java/com/example/android/recyclerview/CustomAdapter.java
+++ RecyclerView/Application/src/main/java/com/example/android/recyclerview/CustomAdapter.java
@@ -18,7 +18,7 @@ package com.example.android.recyclerview;
 
 import com.example.android.common.logger.Log;
 
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
